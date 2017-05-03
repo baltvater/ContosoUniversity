972b941f0f4743aafa10df9b0d4a39fbaf13a671
baltvater <bill.altvater@healthstream.com>
2017-05-03 15:32:41 -0500

    add EF migrations and data attributes

---
+++ ContosoUniversity2_Database/dbo/Tables/Course.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Course] (
+    [CourseId] INT            NOT NULL,
+    [Credits]  INT            NOT NULL,
+    [Title]    NVARCHAR (MAX) NULL,
+    CONSTRAINT [PK_Course] PRIMARY KEY CLUSTERED ([CourseId] ASC)
+);
+
---
+++ ContosoUniversity2_Database/dbo/Tables/Enrollment.sql
@@ -0,0 +1,20 @@
+﻿CREATE TABLE [dbo].[Enrollment] (
+    [EnrollmentId] INT IDENTITY (1, 1) NOT NULL,
+    [CourseId]     INT NOT NULL,
+    [Grade]        INT NULL,
+    [StudentId]    INT NOT NULL,
+    CONSTRAINT [PK_Enrollment] PRIMARY KEY CLUSTERED ([EnrollmentId] ASC),
+    CONSTRAINT [FK_Enrollment_Course_CourseId] FOREIGN KEY ([CourseId]) REFERENCES [dbo].[Course] ([CourseId]) ON DELETE CASCADE,
+    CONSTRAINT [FK_Enrollment_Student_StudentId] FOREIGN KEY ([StudentId]) REFERENCES [dbo].[Student] ([Id]) ON DELETE CASCADE
+);
+
+
+GO
+CREATE NONCLUSTERED INDEX [IX_Enrollment_CourseId]
+    ON [dbo].[Enrollment]([CourseId] ASC);
+
+
+GO
+CREATE NONCLUSTERED INDEX [IX_Enrollment_StudentId]
+    ON [dbo].[Enrollment]([StudentId] ASC);
+
