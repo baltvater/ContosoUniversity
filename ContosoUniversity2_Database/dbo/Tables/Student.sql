d06bf0f8c0d33e9e8d14433a3d8f4b953457c569
baltvater <bill.altvater@healthstream.com>
2017-05-03 17:08:29 -0500

    more data attributes

--- ContosoUniversity2_Database/dbo/Tables/Student.sql
+++ ContosoUniversity2_Database/dbo/Tables/Student.sql
@@ -1,10 +1,12 @@
 ﻿CREATE TABLE [dbo].[Student] (
     [Id]             INT           IDENTITY (1, 1) NOT NULL,
     [EnrollmentDate] DATETIME2 (7) NOT NULL,
-    [FirstMidName]   NVARCHAR (50) NULL,
+    [FirstName]      NVARCHAR (50) NULL,
     [LastName]       NVARCHAR (50) NULL,
     CONSTRAINT [PK_Student] PRIMARY KEY CLUSTERED ([Id] ASC)
 );
+
+
 
 
 
